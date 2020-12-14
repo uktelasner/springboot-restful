a91769ac22799a2c1edfede7bf69387c98b47774
wangcong <2330630904@qq.com>
2020-12-14 09:22:10 +0800

    init-test

--- springboot-restful/src/main/java/com/wencom/restful/RestfulApplication.java
+++ springboot-restful/src/main/java/com/wencom/restful/RestfulApplication.java
@@ -1,8 +1,11 @@
 package com.wencom.restful;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class RestfulApplication {
     public static void main(String[] args) {
-        SpringApplicaion.run(RestfulApplication.class, args);
+        SpringApplication.run(RestfulApplication.class, args);
     }
 }
